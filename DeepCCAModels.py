BugFix: Regularize the normalization to avoid nan issue

DeepCCAModels.py
@@ -56,9 +56,10 @@ class PermNet(nn.Module):
         o1 = self.layers[0](x1)
         o2 = self.layers[0](x2)
         M = self.layers[1](torch.cat((o1,o2),-1))
-        M = M.reshape(-1,8,8)
-        M = F.normalize(M,p=1, dim=1)
-        M = F.normalize(M, p=1, dim=2)
+        M = M.view(-1,8,8)
+        for k in range(1):
+            M = F.normalize(M+1e-12*torch.rand_like(M),p=1, dim=1)
+            M = F.normalize(M+1e-12*torch.rand_like(M), p=1, dim=2)
 
         return M
 
@@ -91,7 +92,7 @@ class DeepCCA(nn.Module):
 
         # flatten
         output1 = output1.view(output1.shape[0],-1)
-        
+
         # print(permutation_out)
 
         # print(f"M: {permutation_out}")

main.py
@@ -66,6 +66,8 @@ class Solver():
             tx2.to(self.device)
 
         train_losses = []
+        Mregs = []
+        corrs = []
         for epoch in range(self.epoch_num):
             epoch_start_time = time.time()
             self.model.train()
@@ -74,41 +76,41 @@ class Solver():
             # print(f"Batch Number: {self.batch_size}")
             for batch_idx in batch_idxs:
                 self.optimizer.zero_grad()
-                # print(f"Batch Indices Size: {len(batch_idx)}")
                 batch_x1 = x1[batch_idx, :]
                 batch_x2 = x2[batch_idx, :]
-                # print(f"Batch Data Number: {len(batch_x1)} {len(batch_x2)}")
                 o1, o2, M = self.model(batch_x1, batch_x2)
-                # print(o1)
-                # print(o2)
-                loss = self.loss(o1, o2, M)
-                print(loss)
+
+                loss, corr, Mreg = self.loss(o1, o2, M)
                 train_losses.append(loss.item())
+                corrs.append(corr.item())
+                Mregs.append(Mreg.item())
                 loss.backward()
 
-                torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1)
+                # torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1)
                 self.optimizer.step()
             train_loss = np.mean(train_losses)
+            corr = np.mean(corrs)
+            Mreg = np.mean(Mregs)
 
-            info_string = "Epoch {:d}/{:d} - time: {:.2f} - training_loss: {:.4f}"
+            info_string = "Epoch {:d}/{:d} - time: {:.2f} - training_loss: {:.4f} Corr: {:.4f} Mreg: {:.4f}"
             if vx1 is not None and vx2 is not None:
                 with torch.no_grad():
                     self.model.eval()
                     val_loss, outputs = self.test(vx1, vx2)
                     info_string += " - val_loss: {:.4f}".format(val_loss)
                     if val_loss < best_val_loss:
-                        self.logger.info(
-                            "Epoch {:d}: val_loss improved from {:.4f} to {:.4f}, saving model to {}".format(epoch + 1, best_val_loss, val_loss, checkpoint))
+                        # self.logger.info(
+                        #     "Epoch {:d}: val_loss improved from {:.4f} to {:.4f}, saving model to {}".format(epoch + 1, best_val_loss, val_loss, checkpoint))
                         best_val_loss = val_loss
                         torch.save(self.model.state_dict(), checkpoint)
-                    else:
-                        self.logger.info("Epoch {:d}: val_loss did not improve from {:.4f}".format(
-                            epoch + 1, best_val_loss))
+                    # else:
+                        # self.logger.info("Epoch {:d}: val_loss did not improve from {:.4f}".format(
+                        #     epoch + 1, best_val_loss))
             else:
                 torch.save(self.model.state_dict(), checkpoint)
             epoch_time = time.time() - epoch_start_time
             self.logger.info(info_string.format(
-                epoch + 1, self.epoch_num, epoch_time, train_loss))
+                epoch + 1, self.epoch_num, epoch_time, train_loss, corr, Mreg))
 
         # train_linear_cca
         if self.linear_cca is not None:
@@ -156,7 +158,7 @@ class Solver():
                 outputs1.append(o1)
                 outputs2.append(o2)
                 Mvalues.append(M)
-                loss = self.loss(o1, o2, M)
+                loss, corr, M_reg = self.loss(o1, o2, M)
                 losses.append(loss.item())
         outputs = [torch.cat(outputs1, dim=0).cpu().numpy(),
                    torch.cat(outputs2, dim=0).cpu().numpy(),
@@ -185,7 +187,7 @@ if __name__ == '__main__':
     k_eigen_check_num = 4
 
     # the parameters for training the network
-    learning_rate = 1e-6
+    learning_rate = 1e-3
     epoch_num = 30
     batch_size = 1024
 

objectives.py
@@ -18,7 +18,7 @@ class cca_loss():
         r1 = 1e-3
         r2 = 1e-3
         eps = 1e-9
-        lambda_M = 0
+        lambda_M = 1e-1
 
         H1, H2 = H1.t(), H2.t()
 
@@ -79,9 +79,7 @@ class cca_loss():
             U = U.topk(self.k_eigen_check_num)[0]
         corr = torch.sum(torch.sqrt(U))
 
-        M_reg = torch.sum(torch.sum(torch.sum(torch.abs(M), dim=2) - (torch.sum(M**2, dim=2))**0.5,dim=1)) +\
-            torch.sum(torch.sum(torch.sum(torch.abs(M), dim=1) - (torch.sum(M**2, dim=1))**0.5,dim=1))
-
+        M_reg = 1/m*( torch.sum(torch.sum(torch.sum(torch.abs(M), dim=2) - (torch.sum(M**2, dim=2))**0.5,dim=1)) +\
+            torch.sum(torch.sum(torch.sum(torch.abs(M), dim=1) - (torch.sum(M**2, dim=1))**0.5,dim=1)))
         
-        
-        return -corr + lambda_M/m * M_reg
+        return -corr + lambda_M * M_reg, corr, M_reg